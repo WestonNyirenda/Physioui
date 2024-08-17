f3d59a49ecbdd98bdb38ac17ed34ae882a6036aa
CAPTAIN_STONEX <jamucathy@gmail.com>
2024-08-17 21:24:46 +0200

    added dashboard content

---
+++ src/components/DataTable.vue
@@ -0,0 +1,108 @@
+<template>
+    <div class="home">
+      <v-row>
+        <v-col>
+          <v-container fluid>
+            <v-card>
+              <v-card-title class="d-flex justify-space-between">
+                <span>Articles</span>
+  
+                <v-dialog width="500">
+                  <template v-slot:activator="{ props }">
+                    <v-btn v-bind="props" text="Add New Article" color="blue"> </v-btn>
+                  </template>
+  
+                  <template v-slot:default="{ isActive }">
+                    <v-card>
+                      <v-card-title>
+                        <span>Enter Article details</span>
+                      </v-card-title>
+                      <v-container fluid>
+                        <v-form>
+                          <v-text-field label="Name" v-model="article.name" required></v-text-field>
+                          <v-text-field label="Article Body" v-model="article.body" required></v-text-field>
+                        </v-form>
+                      </v-container>
+  
+  
+                      <v-card-actions>
+                        <v-spacer></v-spacer>
+  
+                        <v-btn color="orange" text="Close" @click="isActive.value = false"></v-btn>
+  
+                        <v-btn color="green" text="Save" @click="saveArticle"></v-btn>
+                      </v-card-actions>
+                    </v-card>
+                  </template>
+                </v-dialog>
+  
+              </v-card-title>
+              <v-card-text>
+                <v-data-table hover items-per-page="5" v-bind:headers="headers" v-bind:items="articles"
+                  v-bind:loading="articles_loading">
+                  <template v-slot:[`item.actions`]="{ item }">
+  
+                    <v-dialog  ref="updateDialog" width="500">
+                  <template v-slot:activator="{ props }">
+                    <v-icon v-bind="props" color="blue" icon="mdi-pencil" @click="editArticle(item.id)"></v-icon>
+                  </template>
+  
+                  <template v-slot:default="{ }">
+                    <v-card>
+                      <v-card-title>
+                        <span>Edit Article details</span>
+                      </v-card-title>
+                      <v-container fluid>
+                        <v-form>
+                          <v-text-field label="Name" v-model="editArticles.title" required></v-text-field>
+                          <v-text-field label="Article Body" v-model="editArticles.body" required></v-text-field>
+                        </v-form>
+                      </v-container>
+  
+  
+                      <v-card-actions>
+                        <v-spacer></v-spacer>
+  
+  
+                        <v-btn color="green" text="Save" @click="updateArticle(item.id), closeDialog()"></v-btn>
+                      </v-card-actions>
+                    </v-card>
+                  </template>
+                </v-dialog>
+                    
+  
+  
+                    <v-icon color="red" icon="mdi-delete" @click="deleteArticle(item.id)"></v-icon>
+                  </template> 
+                </v-data-table>
+              </v-card-text>
+            </v-card>
+          </v-container>
+        </v-col>
+      </v-row>
+    </div>
+  </template>
+
+<script>
+    export default {
+        name: "DataTable",
+
+        data: () => ({
+            therapists:[{
+                id: 1,
+                first: 'Mercy',
+                last: 'Phiri',
+                Patient: 'suzyo mhango',
+                date : '09:20 AM - 11:00 am'
+            }
+                
+
+            ]
+        })
+        
+    };
+</script>
+
+<style>
+
+</style>
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,40 +1,86 @@
 <template>
+  <v-app>
     <v-container>
       <v-row>
-          <v-col cols="12" sm="12">
-            <v-toolbar flat color="rgba(0,0,0,0)" >
-              <v-toobar-title>Dashboard</v-toobar-title>
+        <v-col cols="12" sm="12">
+          <v-toolbar flat color="rgba(0,0,0,0)">
+            <v-toolbar-title>Dashboard</v-toolbar-title>
+          </v-toolbar>
+          <v-row>
+            <v-col cols="12" md="4" v-for="(card, index) in cards" :key="index">
+              <v-card class="mx-auto" align="center" height="140px" color="#26c6da" outlined>
+                <v-icon color="white" size="50">{{ card.icon }}</v-icon>
+                <v-card-title color="#fffff">{{ card.title }}</v-card-title>
+                <v-card-text color="white">
+                  {{ card.content }}
+                </v-card-text>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+
+      <v-row class="mt-4"> 
+         <v-col>
+          <v-col>
+          <v-card>
+            <v-toolbar color="rgba(0,0,0,0)">
+              <v-toolbar-title>Therapist</v-toolbar-title>
             </v-toolbar>
-            <v-row>
-              <v-col cols="12" md="4" v-for="(card, index) in cards" :key="index">
-                <v-card class="mx-auto" align="center" height="140px" color="#26c6da" outlined >
 
-                  <v-icon color="white" size="50">{{ card.icon }}</v-icon>
+            <v-data-table :items="items">
+              <template v-slot:header="{ column }">
+                {{ column.title.toUpperCase() }}
+              </template>
+            </v-data-table>
 
-                  <v-card-title color="#fffff">{{ card.title }}</v-card-title>
-
-                  <v-card-text color="white">
-                    {{ card.content }}
-                  </v-card-text>
-
-                </v-card>
-              </v-col>
-           </v-row>
-          </v-col>
+          </v-card>
+        </v-col>
+        </v-col>
       </v-row>
     </v-container>
+  </v-app>
 </template>
-
 <script>
 export default {
   name: 'DashboardView',
   data() {
     return {
       cards: [
-        { title: 'Appointments', content: '11', icon: 'mdi-calendar'},
+        { title: 'Appointments', content: '11', icon: 'mdi-calendar' },
         { title: 'Therapists', content: '21', icon: 'fas fa-user-md' },
         { title: 'Patients', content: ' 3', icon: 'fas fa-hospital' },
       ],
+
+      items: [
+        {
+          id: 1,
+          FirstName: 'Vinjero',
+          LastName: 'Soko',
+          Role: 'Supervisor',
+          Appointment: '9:50 - 11:45',
+          
+        },
+        {
+          id: 2,
+          FirstName: 'Gomezyani',
+          LastName: 'Nyasulu',
+          Role: 'Trainee_therapist',
+          Appointment: '16:20 - 16:45',
+          
+        },
+        {
+          id: 3,
+          FirstName: 'Wanidofya',
+          LastName: 'msiska',
+          Role: 'therapist',
+          Appointment: '10:00 - 11:45',
+          
+        },
+      
+        
+      ],
+    
     };
   },
 };
